9042e63e1b762f5f464fa25035b7961a36d41129
nguyengiang1907 <ng190704@gmail.com>
2023-02-09 09:29:51 +0700

    Buổi 1

--- src/tinh_nam_nhuan.java
+++ src/tinh_nam_nhuan.java
@@ -1,22 +1,19 @@
-//import java.util.Scanner;
-//
-//public class tinh_nam_nhuan {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int year ;
-//        if(year % 4 == 0){
-//            if(year % 100 == 0){
-//                if(year % 400 == 0){
-//                    System.out.printf("%d is a leap year", year);
-//                } else {
-//                    System.out.printf("%d is NOT a leap year", year);
-//                }
-//            } else {
-//                System.out.printf("%d is a leap year", year);
-//            }
-//        } else {
-//            System.out.printf("%d is NOT a leap year", year);
-//        }
-//    }
-//    }
+import java.util.Scanner;
+
+public class tinh_nam_nhuan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        year = sc.nextInt();
+        if (year % 4== 0 & year % 100 !=0){
+            System.out.println("Năm nhuận:");
+        } else if (year % 100 ==0 & year % 400 ==0 ) {
+            System.out.println("Năm nhuận:");
+        } else if (year % 100==0 & year % 400 != 0) {
+            System.out.println("Không phải năm nhuận :");
+        } else {
+            System.out.println("Không phải năm nhuận :");
+        }
+    }
+    }
 
--- src/tinh_phuong_trinh_bac_1.java
+++ src/tinh_phuong_trinh_bac_1.java
@@ -5,6 +5,5 @@ public class tinh_phuong_trinh_bac_1 {
 //        Scanner sc = new Scanner(System.in);
 //
 //    }
-int a = 5; int b = 7; int c = a++
 
 }
